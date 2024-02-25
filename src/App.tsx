bcd5e2f2c4dd9d3ed8a02fda75ebff19599313a2
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-02-26 00:43:34 +0330

    setup and init prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -9,6 +9,19 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+            <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
+              Slow-carb next level shoindxgoitch ethical authentic, scenester
+              sriracha forage.
+            </h1>
+            <button className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">
+              Button
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
