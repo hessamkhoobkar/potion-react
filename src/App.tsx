e4780625b516ac1f2ca01496e195ad662c011206
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-02-26 04:07:54 +0330

    Update component aliases and import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 // import { useState } from "react";
-import { Button } from '@/src/components/ui/button';
+import { Button } from '@/components/ui/button';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="w-dvw h-dvh flex justify-center items-center">
-        <section className="">
+      <div className="flex h-dvh w-dvw items-stretch justify-center">
+        <aside className="w-1/3 bg-gray-200 p-8">
           <Button>Click me</Button>
+        </aside>
+        <section className="w-2/3 p-8">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+            iusto corrupti vero error, dolorem tempora totam fugiat quis
+            voluptates quod quo exercitationem ea inventore ullam, assumenda
+            omnis amet accusantium numquam.
+          </p>
         </section>
       </div>
     </>
