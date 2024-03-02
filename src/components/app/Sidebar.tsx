9880c32f0a1189af3c6523e8c7a6827b56b24653
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-03-02 23:35:40 +0330

    init base account section

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -1,15 +1,19 @@
+import AccountSection from './sidebar/AccountSection';
 import ChatHistory from './sidebar/ChatHistory';
 
 export default function Sidebar() {
   return (
     <div className="z-10 flex h-dvh w-72 flex-col overflow-hidden">
       <div className="flex h-24 w-full items-center justify-start p-4 ps-8">
-        <span>Sidebar Logo</span>
+        <span className="text-2xl font-black">
+          <span>Potion</span>
+          <span className="ms-2 inline-block bg-gradient-to-tr from-secondary via-primary to-primary bg-clip-text text-transparent">
+            AI
+          </span>
+        </span>
       </div>
       <ChatHistory />
-      <div className="w-full flex-col border-t p-4 pe-2 pt-3">
-        <span>Sidebar user card</span>
-      </div>
+      <AccountSection />
     </div>
   );
 }
