8edcd5f8d558784ba8d9278499bb3e3540c453dc
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-03-07 21:22:05 +0330

    abstract the logo section

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -1,17 +1,11 @@
 import AccountSection from './sidebar/AccountSection';
+import Branding from './sidebar/Branding';
 import ChatHistory from './sidebar/ChatHistory';
 
 export default function Sidebar() {
   return (
     <div className="z-10 flex h-dvh w-72 flex-shrink-0 flex-col overflow-hidden">
-      <div className="flex h-24 w-full items-center justify-start p-4 ps-8">
-        <span className="text-2xl font-black">
-          <span>Potion</span>
-          <span className="ms-2 inline-block bg-gradient-to-tr from-secondary via-primary to-primary bg-clip-text text-transparent">
-            AI
-          </span>
-        </span>
-      </div>
+      <Branding />
       <ChatHistory />
       <AccountSection />
     </div>
---
+++ src/components/app/sidebar/Branding.tsx
@@ -0,0 +1,12 @@
+export default function Branding() {
+  return (
+    <div className="flex h-24 w-full items-center justify-start p-4 ps-8">
+      <span className="text-2xl font-black">
+        <span>Potion</span>
+        <span className="ms-2 inline-block bg-gradient-to-tr from-secondary via-primary to-primary bg-clip-text text-transparent">
+          AI
+        </span>
+      </span>
+    </div>
+  );
+}
